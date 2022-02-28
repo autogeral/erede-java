f219f906cfe33289b09957fde99656e3f6fe1c69
Yannops <yannsilveira34@gmail.com>
2022-02-28 13:50:27 -0300

    Alteração da classe TransactionResponse para torná-la serializável.

--- src/main/java/br/com/userede/erede/TransactionResponse.java
+++ src/main/java/br/com/userede/erede/TransactionResponse.java
@@ -1,5 +1,9 @@
 package br.com.userede.erede;
 
-public class TransactionResponse extends AbstractTransaction<Capture> {
+import java.io.Serializable;
 
+public class TransactionResponse extends AbstractTransaction<Capture> implements Serializable {
+
+
+    private static final long serialVersionUID = -8338356417266359320L;
 }
